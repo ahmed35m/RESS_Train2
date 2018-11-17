73752052b50f3dd1e7c6a387e76644f58604eaed
Shamsco <a_shamiss@hotmail.com>
2018-11-17 13:47:47 -0600

    Create Cart.java

---
+++ src/TrainManagement/Cart.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package TrainManagement;
+
+/**
+ * @author Ahmed
+ *
+ */
+public class Cart {
+	//Cart Length in Meters
+	private double length;
+	
+	public Cart()
+	{
+		this.length = 0;
+	}
+	
+	public double getLength()
+	{
+		return length;
+	}
+	public void setLength(double length)
+	{
+		this.length = length;
+	}
+
+}
