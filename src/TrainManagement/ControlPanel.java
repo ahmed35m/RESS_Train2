2fecc897f965997d0b8f09cee580e5cd422d125d
Shamsco <a_shamiss@hotmail.com>
2018-11-17 13:04:12 -0600

    Init

---
+++ src/TrainManagement/ControlPanel.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package TrainManagement;
+
+/**
+ * @author Ahmed
+ *
+ */
+public class ControlPanel {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
